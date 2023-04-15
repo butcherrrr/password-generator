9bd1b1a5e3c66fd0a2ae5182d68489ea2930fd1e
anthony <anthony.butcher@samblagroup.com>
2023-04-15 18:08:43 +0200

    Improvements

--- pass.js
+++ pass.js
@@ -1,41 +1,37 @@
 const args = process.argv.slice(2);
 
-const MAX_PASS_LENGTH = args[0] || 21;
-const MIN_PASS_LENGTH = 6;
-
-const MULTI_CASE_TYPES = ['LETTER'];
+const MAX_PASSWORD_LENGTH = args[0] || 21;
+const MIN_PASSWORD_LENGTH = 6;
+const MIN_PASSWORD_LENGTH_ERROR = 'Password must be minimum six characters';
 
 const CHARACTER_TYPES = {
   LETTER: 'abcdefghijklmnopqrstuvwxyz',
   NUMBER: '1234567890',
   SYMBOL: '!"#€%&£$',
 };
-
-const randomize = (characterType, capitalize = false) => {
-  const length = characterType.length;
-  return capitalize
-    ? characterType[Math.floor(Math.random() * length)].toUpperCase()
-    : characterType[Math.floor(Math.random() * length)];
-};
+const MULTI_CASE_TYPE = 'LETTER';
 
 const gen = () => {
-  let pass = '';
-  let capitalize;
-
-  if (MAX_PASS_LENGTH < MIN_PASS_LENGTH) {
-    return 'Password must be minimum six characters';
+  if (MAX_PASSWORD_LENGTH < MIN_PASSWORD_LENGTH) {
+    return MIN_PASSWORD_LENGTH_ERROR;
   }
 
-  while (pass.length < MAX_PASS_LENGTH) {
+  const randomize = (characterType, capitalize) => {
+    const character = characterType[Math.floor(Math.random() * characterType.length)];
+    return capitalize ? character.toUpperCase() : character;
+  };
+
+  let password = '';
+  while (password.length < MAX_PASSWORD_LENGTH) {
     for (const [key, value] of Object.entries(CHARACTER_TYPES)) {
-      if (MULTI_CASE_TYPES.includes(key)) capitalize = Math.random() < 0.5;
-      pass += randomize(value, capitalize);
+      password += randomize(value, key === MULTI_CASE_TYPE && Math.random() < 0.5);
     }
   }
 
-  return pass;
+  return password;
 };
 
-const res = gen();
-console.log(res);
+const result = gen();
+
+console.log(result);
 
